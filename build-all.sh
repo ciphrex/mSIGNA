3a8ac9956f6253bbd33559d53f8377410246f303
Eric Lombrozo <elombrozo@gmail.com>
2013-12-28 21:45:08 -0800

    Script for building CoinVault project.

---
+++ build-all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+case $1 in
+linux)
+    SPEC=""
+;;
+
+mingw64)
+    SPEC="-spec win32-g++"
+;;
+
+*)
+    echo "You must specify between linux and mingw64."
+    exit
+esac
+
+CURRENT_DIR=$(pwd)
+
+set -x
+
+cd deps/CoinClasses
+make OS=$1
+
+cd ../CoinQ
+make OS=$1
+
+cd ../sqlite3
+make OS=$1
+
+cd $CURRENT_DIR
+qmake $SPEC && make
