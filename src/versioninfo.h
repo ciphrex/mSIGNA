6c72e5a46774764eb9e67e3976140e7fae3f80ba
Eric Lombrozo <elombrozo@gmail.com>
2014-02-17 20:07:46 -0800

    Version bump.

--- src/versioninfo.h
+++ src/versioninfo.h
@@ -16,6 +16,6 @@
 const int VERSIONPADDINGRIGHT = 20;
 const int VERSIONPADDINGBOTTOM = 30;
 
-const QString VERSIONTEXT("Version 0.0.8 alpha");
+const QString VERSIONTEXT("Version 0.0.9 alpha");
 
 #endif //  VAULT_VERSIONINFO_H
