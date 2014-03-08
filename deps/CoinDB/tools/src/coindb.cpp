8dbdceea43f18b8f5d7278126ebde4bc952a2d24
Eric Lombrozo <elombrozo@gmail.com>
2014-03-07 20:34:56 -0800

    WIP

--- deps/CoinDB/tools/src/coindb.cpp
+++ deps/CoinDB/tools/src/coindb.cpp
@@ -9,11 +9,36 @@
 
 #include <cli.hpp>
 
+#include <Database.hxx>
+#include <Vault.h>
+
+#include <odb/database.hxx>
+#include <odb/transaction.hxx>
+
 #include <iostream>
+#include <sstream>
+
+using namespace odb::core;
+using namespace CoinDB;
+
+cli::result_t cmd_create(bool bHelp, const cli::params_t& params)
+{
+    if (bHelp || params.size() != 1) {
+        return "create <filename> - create a new vault.";
+    }
+
+    std::unique_ptr<database> db(openDatabase(params[0], true));
+
+    std::stringstream ss;
+    ss << "Vault " << params[0] << " created.";
+    return ss.str();
+}
 
 int main(int argc, char* argv[])
 {
     cli::command_map cmds("CoinDB by Eric Lombrozo v0.2.0");
+    cmds.add("create", &cmd_create);
+
     try 
     {
         return cmds.exec(argc, argv);
