32f657aa2cc11878d480e968452d81f896b96e46
Eric Lombrozo <elombrozo@gmail.com>
2014-03-22 22:59:01 -0700

    sort signing scripts and use REQUESTED | RECEIVED as default.

--- deps/CoinDB/src/Vault.cpp
+++ deps/CoinDB/src/Vault.cpp
@@ -380,6 +380,7 @@ std::vector<SigningScriptView> Vault::getSigningScriptViews(const std::string& a
     query_t query(query_t::SigningScript::status.in_range(statusRange.begin(), statusRange.end()));
     if (!account_name.empty()) query += query_t::Account::name == account_name;
     if (!bin_name.empty()) query += query_t::AccountBin::name == bin_name;
+    query += "ORDER BY" + query_t::Account::name + "ASC," + query_t::AccountBin::name + "ASC," + query_t::SigningScript::status + "DESC";
 
     boost::lock_guard<boost::mutex> lock(mutex);
     odb::core::session s;
