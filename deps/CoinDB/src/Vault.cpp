2b92366d7cdba19c251b37e02e7b6161b9480b7f
Eric Lombrozo <elombrozo@gmail.com>
2014-03-25 03:58:20 -0700

    Removed SQL tracer.

--- deps/CoinDB/src/Vault.cpp
+++ deps/CoinDB/src/Vault.cpp
@@ -519,8 +519,6 @@ std::vector<TxOutView> Vault::getTxOutViews(const std::string& account_name, con
 
     boost::lock_guard<boost::mutex> lock(mutex);
     odb::core::transaction t(db_->begin());
-    t.tracer(odb::stderr_tracer);
-
     std::vector<TxOutView> views;
     odb::result<TxOutView> r(db_->query<TxOutView>(query));
     for (auto& view: r) { views.push_back(view); }
