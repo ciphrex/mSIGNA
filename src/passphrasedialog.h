38125bbba28129da8c9ef035c07cfe8cd2b06ef1
Eric Lombrozo <elombrozo@gmail.com>
2014-03-30 13:53:23 -0700

    Added passphrase dialog to project.

---
+++ src/passphrasedialog.h
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// CoinVault
+//
+// passphrasedialog.h
+//
+// Copyright (c) 2014 Eric Lombrozo
+//
+// All Rights Reserved.
+
+#pragma once
+
+class QLineEdit;
+
+#include <QDialog>
+
+#include <CoinQ_typedefs.h>
+
+class PassphraseDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    PassphraseDialog(const QString& prompt, QWidget* parent = NULL);
+
+    QString getPassphrase() const;
+
+private:
+    QTextEdit* passphraseEdit;
+};
