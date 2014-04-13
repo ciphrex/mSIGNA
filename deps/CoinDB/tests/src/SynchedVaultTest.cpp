b1c3324ecfd3e9537488fecfc5c6701e15c1fc0a
Eric Lombrozo <elombrozo@gmail.com>
2014-04-13 04:07:29 -0700

    SynchedVaultTest

---
+++ deps/CoinDB/tests/src/SynchedVaultTest.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// SynchedVaultTest.cpp
+//
+// Copyright (c) 2014 Eric Lombrozo
+//
+// All Rights Reserved.
+//
+
+#include <SynchedVault.h>
+#include <logger.h>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    INIT_LOGGER("SynchedVaultTest.log");
+    return 0;
+
+}
