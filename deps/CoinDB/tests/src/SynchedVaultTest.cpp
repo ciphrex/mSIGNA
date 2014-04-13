fccd794ea38cbcfcfc6e30ae5a6c7f6543555c9a
Eric Lombrozo <elombrozo@gmail.com>
2014-04-13 04:25:48 -0700

    WIP

--- deps/CoinDB/tests/src/SynchedVaultTest.cpp
+++ deps/CoinDB/tests/src/SynchedVaultTest.cpp
@@ -11,10 +11,57 @@
 #include <logger.h>
 
 #include <iostream>
+#include <signal.h>
+
+#include <thread>
+#include <chrono>
+
+using namespace CoinDB;
+using namespace std;
+
+bool g_bShutdown = false;
+
+void finish(int sig)
+{
+    LOGGER(debug) << "Stopping..." << endl;
+    g_bShutdown = true;
+}
 
 int main(int argc, char* argv[])
 {
+    if (argc != 4)
+    {
+        cout << "Usage: " << argv[0] << " [vault file] [p2p host] [p2p port]" << endl;
+        return 0;
+    }
+
+    string filename(argv[1]);
+    string host(argv[2]);
+    string port(argv[3]);
+        
     INIT_LOGGER("SynchedVaultTest.log");
+
+    signal(SIGINT, &finish);
+
+    SynchedVault synchedVault;
+
+    try
+    {
+        LOGGER(debug) << "Opening vault " << filename << endl;
+        synchedVault.openVault(filename);
+        LOGGER(debug) << "Attempting to sync with " << host << ":" << port << endl;
+        synchedVault.startSync(host, port);
+    }
+    catch (const std::exception& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+
+    while (!g_bShutdown) { std::this_thread::sleep_for(std::chrono::microseconds(200)); }
+
+    LOGGER(debug) << "Stopping..." << endl;
+
     return 0;
 
 }
