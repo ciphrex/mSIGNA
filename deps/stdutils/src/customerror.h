dac57cb1824655a05cf2c1de3eeda749dd025da4
Eric Lombrozo <elombrozo@gmail.com>
2014-06-26 21:24:44 -0700

    stdutils::custom_error::has_code()

--- deps/stdutils/src/customerror.h
+++ deps/stdutils/src/customerror.h
@@ -17,17 +17,28 @@ namespace stdutils
 class custom_error : public std::runtime_error
 {
 public:
-    explicit custom_error(const std::string& what, int code = 0)
-        : std::runtime_error(what), code_(code) { }
+    explicit custom_error(const std::exception& e)
+        : std::runtime_error(e.what()), has_code_(false), code_(0) { }
+ 
+    explicit custom_error(const std::string& what)
+        : std::runtime_error(what), has_code_(false), code_(0) { }
 
-    explicit custom_error(const char* what, int code = 0)
-        : std::runtime_error(what), code_(code) { }
+    explicit custom_error(const char* what)
+        : std::runtime_error(what), has_code_(false), code_(0) { }
+
+    custom_error(const std::string& what, int code)
+        : std::runtime_error(what), has_code_(true), code_(code) { }
+
+    custom_error(const char* what, int code)
+        : std::runtime_error(what), has_code_(true), code_(code) { }
 
     virtual ~custom_error() throw() { }
 
+    bool has_code() const { return has_code_; }
     int code() const { return code_; }
 
 private:
+    bool has_code_;
     int code_;
 };
 
