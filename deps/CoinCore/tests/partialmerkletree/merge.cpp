d6840b5313414c1314ac550e46d3a415b03a0558
Eric Lombrozo <elombrozo@gmail.com>
2014-06-30 22:02:55 -0700

    PartialMerkleTree::merge() unit test.

--- deps/CoinCore/tests/partialmerkletree/merge.cpp
+++ deps/CoinCore/tests/partialmerkletree/merge.cpp
@@ -12,17 +12,6 @@ int main()
         std::vector<PartialMerkleTree::MerkleLeaf> leaves1;
         std::vector<PartialMerkleTree::MerkleLeaf> leaves2;
 
-/*
-        leaves.push_back(make_pair(uchar_vector("cf86811c2853a14c520d7bc7cd2f41e16ba1d02a19ddef197df8fe4c575a599e").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("da9219371684385a997194b54ee7cbe908eb829043e1cb245b09157a2adb5de3").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("87c9b40548e71b0c50fc535aead2674a3f575f18af451b3f27770e04bf03e3d1").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("757efcca85025b9b67780e6d66f4284badf01c9d3eb1a6f4648d57d383868625").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("123ec576f0cc12c5e3876c82b4f860ac7f6170096a089982b99d24e575dc521b").getReverse(), true));
-        leaves.push_back(make_pair(uchar_vector("d52a468b14a3b2dfa11eb26081aa2e0b7158986118f3021c7969f1c675e385a9").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("98abb76a0289477519b98ef216dbfb5fe807a90bb9a7f53a140e2d0213e38c80").getReverse(), false));
-        leaves.push_back(make_pair(uchar_vector("0b82afba1b61e301ade9f67bd588ced909967156084bd6b4c088cc5b266c099b").getReverse(), true));
-
-    */
         leaves1.push_back(make_pair(uchar_vector("51ad11ed9bad5760329d771cd889f85e5c17b9236f7d42c6404ba41eb0ff0167").getReverse(), false));
         leaves1.push_back(make_pair(uchar_vector("56ecfac584945699e2cfccbc6989a6ab61abeacefc65ecfadeb0381e75470b4e").getReverse(), false));
         leaves1.push_back(make_pair(uchar_vector("746be68a8a21cf6f0ef26a9c7bb1339e224d308d8dd208784ca55a76fcf6c38f").getReverse(), false));
@@ -83,6 +72,10 @@ int main()
 
         cout << "Tree2" << endl;
         cout << tree2.toIndentedString() << endl;
+
+        tree1.merge(tree2);
+        cout << "Tree1 + Tree2" << endl;
+        cout << tree1.toIndentedString() << endl;
 
         return 0;
     }
