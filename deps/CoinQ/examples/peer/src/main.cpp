9e9e2d27ad091a43d6cd0b6b38d9ebdd6841ca9d
Eric Lombrozo <elombrozo@gmail.com>
2014-07-29 22:32:46 -0700

    Added SIGTERM handler to CoinQ peer example.

--- deps/CoinQ/examples/peer/src/main.cpp
+++ deps/CoinQ/examples/peer/src/main.cpp
@@ -86,6 +86,7 @@ int main(int argc, char* argv[])
     if (argc > 1) { host = argv[1]; }
 
     signal(SIGINT, &finish);
+    signal(SIGTERM, &finish);
 
     CoinParams coinParams = getBitcoinParams();
 
