dfad6cb443cf24faf6083659d69b78c679b01ddf
Eric Lombrozo <elombrozo@gmail.com>
2014-08-05 18:41:55 -0700

    netsync example updated.

--- deps/CoinQ/examples/netsync/src/main.cpp
+++ deps/CoinQ/examples/netsync/src/main.cpp
@@ -10,7 +10,9 @@
 
 #include <CoinCore/CoinNodeData.h>
 #include <CoinCore/MerkleTree.h>
+#include <CoinCore/BloomFilter.h>
 #include <CoinCore/typedefs.h>
+#include <CoinCore/random.h>
 
 #include <CoinQ/CoinQ_netsync.h>
 #include <CoinQ/CoinQ_coinparams.h>
@@ -41,14 +43,18 @@ int main(int argc, char* argv[])
 
         if (argc < 3)
         {
-            cerr << "# Usage: " << argv[0] << " <network> <host> [port]" << endl
+            cerr << "# Usage: " << argv[0] << " <network> <host> [port] [bloom filter elements = 10]" << endl
                  << "# Supported networks: " << stdutils::delimited_list(networkSelector.getNetworkNames(), ", ") << endl;
             return -1;
         }
 
+        cout << endl << "Initializing logger to file netsync.log..." << endl;
+        INIT_LOGGER("netsync.log");
+
         CoinParams coinParams = networkSelector.getCoinParams(argv[1]);
         string host = argv[2];
         string port = (argc > 3) ? argv[3] : coinParams.default_port();
+        unsigned int filterElements = (argc > 4) ? strtoul(argv[4], NULL, 0) : 10;
 
         cout << endl << "Connecting to " << coinParams.network_name() << " peer" << endl
              << "-------------------------------------------" << endl
@@ -58,11 +64,7 @@ int main(int argc, char* argv[])
              << "  protocol version: " << dec << coinParams.protocol_version() << endl
              << endl;
 
-        INIT_LOGGER("netsync.log");
-        LOGGER(info) << endl << endl << endl << endl << endl << endl;
-    
-        signal(SIGINT, &finish);
-        signal(SIGTERM, &finish);
+        unsigned int blockTxIndex = 0;
 
         Network::NetworkSync networkSync(coinParams);
         networkSync.loadHeaders("blocktree.dat", false, [&](const CoinQBlockTreeMem& blocktree) {
@@ -123,16 +125,39 @@ int main(int argc, char* argv[])
             cout << "NetworkSync status: " << status << endl;
         });
 
-        networkSync.subscribeTx([&](const Transaction& tx) {
-            cout << "NetworkSync transaction:" << tx.getHashLittleEndian().getHex() << endl;
+        networkSync.subscribeNewTx([&](const Transaction& tx) {
+            cout << endl << "NEW TX: " << tx.getHashLittleEndian().getHex() << endl;
+        });
+
+        networkSync.subscribeMerkleTx([&](const ChainMerkleBlock& merkleBlock, const Transaction& tx, unsigned int txIndex, unsigned int txTotal)
+        {
+            cout << "  tx (" << txIndex << "/" << (txTotal - 1) << "): " << tx.getHashLittleEndian().getHex() << endl;
         });
 
         networkSync.subscribeBlock([&](const ChainBlock& block) {
-            cout << "NetworkSync block: " << block.blockHeader.getHashLittleEndian().getHex() << " height: " << block.height << endl;
+            cout << "NEW BLOCK: " << block.blockHeader.getHashLittleEndian().getHex() << " height: " << block.height << endl;
         });
 
         networkSync.subscribeMerkleBlock([&](const ChainMerkleBlock& merkleblock) {
-            cout << "NetworkSync merkle block: " << merkleblock.blockHeader.getHashLittleEndian().getHex() << " height: " << merkleblock.height << endl;
+            cout << endl << "NEW MERKLE BLOCK" << endl
+                         << "  hash: " << merkleblock.blockHeader.getHashLittleEndian().getHex() << endl
+                         << "  height: " << merkleblock.height << endl;
+
+            try
+            {
+                PartialMerkleTree tree(merkleblock.nTxs, merkleblock.hashes, merkleblock.flags, merkleblock.blockHeader.merkleRoot);
+                std::vector<uchar_vector> txhashes = tree.getTxHashesLittleEndianVector();
+                unsigned int i = 0;
+                cout << "should contain txs:" << endl;
+                for (auto& txhash: txhashes) { cout << "  tx " << i++ << ": " << uchar_vector(txhash).getHex() << endl; }
+            }
+            catch (const exception& e)
+            {
+                cout << "Error constructing partial merkle tree: " << e.what() << endl;
+            }
+
+            cout << "--------------------" << endl;
+            blockTxIndex = 0;
         });
 
         networkSync.subscribeAddBestChain([&](const ChainHeader& header) {
@@ -147,16 +172,39 @@ int main(int argc, char* argv[])
             cout << "NetworkSync block tree changed." << endl;
         });
 
+        // Set the bloom filter
+        cout << endl << "Bloom filter settings" << endl
+                     << "---------------------" << endl
+                     << "  elements:            " << filterElements << endl
+                     << "  false positive rate: 0.001" << endl
+                     << "  nTweak:              0" << endl
+                     << "  nFlags:              0" << endl;
+
+        BloomFilter filter(filterElements, 0.001, 0, 0);
+        for (unsigned int i = 0; i < filterElements; i++) { filter.insert(random_bytes(32)); }
+        networkSync.setBloomFilter(filter);
+
+        cout << endl << "Registering SIGINT and SIGTERM signal handlers.." << endl;
+        signal(SIGINT, &finish);
+        signal(SIGTERM, &finish);
+
+        LOGGER(info) << endl << endl << endl << endl << endl << endl;
+        cout << endl << "Starting sync..." << endl;
         LOGGER(info) << "Starting..." << endl;
         networkSync.start(host, port);
         while (!g_bShutdown) { usleep(200); }
 
+        cout << "Stopping..." << endl;
         LOGGER(info) << "Stopping..." << endl;
         networkSync.stop();
+
+        cout << "Stopped." << endl;
+        LOGGER(info) << "Stopped." << endl;
     }
     catch (const exception& e)
     {
-        cerr << "Error: " << e.what() << endl;
+        cerr << "Unexpected termination. Error: " << e.what() << endl;
+        LOGGER(error) << "Unexpected termination. Error: " << e.what() << endl;
         return -2;
     }
 
