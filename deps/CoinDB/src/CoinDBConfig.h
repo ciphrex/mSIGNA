ffc1f6d8dd3adb1b58db2890462c02ed85ac4cc6
Eric Lombrozo <elombrozo@gmail.com>
2014-08-07 22:12:53 -0700

    WIP

--- deps/CoinDB/src/CoinDBConfig.h
+++ deps/CoinDB/src/CoinDBConfig.h
@@ -29,7 +29,7 @@ public:
     virtual ~CoinDBConfig() { }
 
     std::string getHelpOptions() const;
-    bool parseParams(int argc, char* argv[]);
+    virtual bool parseParams(int argc, char* argv[]);
 
     const std::string& getDataDir() const { return m_dataDir; }
     const std::string& getConfigFile() const { return m_configFile; }
@@ -38,6 +38,7 @@ public:
 
 protected:
     boost::program_options::options_description m_options;
+    boost::program_options::variables_map m_vm;
 
     std::string m_dataDir;
     std::string m_configFile;
@@ -68,26 +69,29 @@ inline std::string CoinDBConfig::getHelpOptions() const
 inline bool CoinDBConfig::parseParams(int argc, char* argv[])
 {
     namespace po = boost::program_options;
-    namespace fs = boost::filesystem;
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, m_options), vm);
-    po::notify(vm);
+    po::store(po::parse_command_line(argc, argv, m_options), m_vm);
+    po::notify(m_vm);
 
-    if (vm.count("help")) return false;
+    if (m_vm.count("help")) return false;
 
     using namespace sysutils::filesystem;
-    if (!vm.count("datadir"))       { m_dataDir = getDefaultDataDir(DEFAULT_DATA_DIR); }
-    if (!vm.count("config"))        { m_configFile = m_dataDir + "/" + DEFAULT_CONFIG_FILE; }
+    if (!m_vm.count("datadir"))     { m_dataDir = getDefaultDataDir(DEFAULT_DATA_DIR); }
+    if (!m_vm.count("config"))      { m_configFile = m_dataDir + "/" + DEFAULT_CONFIG_FILE; }
     else                            { m_configFile = m_dataDir + "/" + m_configFile; }
+
+    namespace fs = boost::filesystem;
+    fs::path dataDirPath(m_dataDir);
+    if ((!fs::exists(dataDirPath) && !fs::create_directory(dataDirPath)) || !fs::is_directory(dataDirPath))
+        throw std::runtime_error("Invalid datadir.");
 
     fs::path p(m_configFile);
     if (fs::exists(p))
     {
         std::ifstream ifs(m_configFile);
-        po::store(po::parse_config_file(ifs, m_options), vm);
+        po::store(po::parse_config_file(ifs, m_options), m_vm);
         ifs.close();
-        po::notify(vm); 
+        po::notify(m_vm); 
     }
 
     return true;
