da239f9c2ae84702c2864bc8c35207ba0ef07034
Eric Lombrozo <elombrozo@gmail.com>
2014-08-16 04:48:47 -0700

    odb include path

--- deps/mk/odb.mk
+++ deps/mk/odb.mk
@@ -11,3 +11,14 @@ else
     $(error DB must be set to sqlite or mysql)
 endif
 
+ifdef LOCAL_SYSROOT
+    ODB_INCLUDE_PATH += -I$(LOCAL_SYSROOT)/include
+endif
+
+ifdef SYSROOT
+    ifeq ($(OS), mingw64)
+        # ODB include path cannot include windows system headers when doing crossbuilds
+        ODB_INCLUDE_PATH += -I$(SYSROOT)/local/include
+    endif
+endif
+
