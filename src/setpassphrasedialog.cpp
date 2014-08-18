584c1d80168a14e23eed4ca66ae490f84123d705
Eric Lombrozo <elombrozo@gmail.com>
2014-08-18 15:14:46 -0700

    Added SetPassphraseDialog

---
+++ src/setpassphrasedialog.cpp
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// CoinVault
+//
+// setpassphrasedialog.cpp
+//
+// Copyright (c) 2014 Eric Lombrozo
+//
+// All Rights Reserved.
+
+#include "setpassphrasedialog.h"
+
+#include <QDialogButtonBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QLabel>
+
+SetPassphraseDialog::SetPassphraseDialog(const QString& prompt, QWidget* parent)
+    : QDialog(parent)
+{
+    // Buttons
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
+                                     | QDialogButtonBox::Cancel);
+
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+
+    // Prompt
+    QLabel* promptLabel = new QLabel();
+    promptLabel->setText(prompt);
+
+    // Text Edits
+    passphrase1Edit = new QLineEdit();
+    passphrase1Edit->setEchoMode(QLineEdit::Password);
+    passphrase2Edit = new QLineEdit();
+    passphrase2Edit->setEchoMode(QLineEdit::Password);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout();
+    mainLayout->setSizeConstraint(QLayout::SetNoConstraint);
+    mainLayout->addWidget(promptLabel);
+    mainLayout->addWidget(passphrase1Edit);
+    mainLayout->addWidget(passphrase2Edit);
+    mainLayout->addWidget(buttonBox);
+    setLayout(mainLayout);
+
+    resize(500, 140);
+}
+
+QString SetPassphraseDialog::getPassphrase() const
+{
+    if (passphrase1Edit->text() != passphrase2Edit->text())
+        throw std::runtime_error("Passphrases do not match.");
+
+    // TODO: check passphrase strength
+
+    return passphrase1Edit->text();
+}
---
+++ src/setpassphrasedialog.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// CoinVault
+//
+// setpassphrasedialog.h
+//
+// Copyright (c) 2014 Eric Lombrozo
+//
+// All Rights Reserved.
+
+#pragma once
+
+class QLineEdit;
+
+#include <QDialog>
+
+#include <CoinQ/CoinQ_typedefs.h>
+
+class SetPassphraseDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    SetPassphraseDialog(const QString& prompt, QWidget* parent = NULL);
+
+    QString getPassphrase() const;
+
+private:
+    QLineEdit* passphrase1Edit;
+    QLineEdit* passphrase2Edit;
+};
