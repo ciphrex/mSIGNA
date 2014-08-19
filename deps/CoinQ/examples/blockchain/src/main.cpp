5a73eecf2731338ae0558f90ac39bffe562466d9
Eric Lombrozo <elombrozo@gmail.com>
2014-08-18 19:04:14 -0700

    blockchain example accepts starting block.

--- deps/CoinQ/examples/blockchain/src/main.cpp
+++ deps/CoinQ/examples/blockchain/src/main.cpp
@@ -35,14 +35,20 @@ int main(int argc, char* argv[])
 
         if (argc < 3)
         {
-            cerr << "# Usage: " << argv[0] << " <network> <host> [port]" << endl
+            cerr << "# Usage: " << argv[0] << " <network> <host> [start hash] [start height] [port]" << endl
                  << "# Supported networks: " << stdutils::delimited_list(networkSelector.getNetworkNames(), ", ") << endl;
             return -1;
         }
 
         CoinParams coinParams = networkSelector.getCoinParams(argv[1]);
         string host = argv[2];
-        string port = (argc > 3) ? argv[3] : coinParams.default_port();
+
+        vector<uchar_vector> locatorHashes;
+        if (argc > 3) { locatorHashes.push_back(uchar_vector(argv[3])); }
+
+        int startHeight = (argc > 4) ? strtoll(argv[4], NULL, 0) : 0;
+
+        string port = (argc > 5) ? argv[5] : coinParams.default_port();
 
         Network::BlockchainDownload download(coinParams);
 
@@ -61,7 +67,7 @@ int main(int argc, char* argv[])
         download.subscribeTimeout([&]()         { cout << "Peer timed out." << endl; });
         download.subscribeConnectionError([&](const string& error, int /*code*/) { cout << "Connection error: " << error << endl; });
 
-        download.subscribeBlock([&](const Coin::CoinBlock& block)    { cout << "Block - hash: " << block.hash().getHex() << " height: " << download.getBestHeight() << endl; });
+        download.subscribeBlock([&](const Coin::CoinBlock& block)    { cout << "Block - hash: " << block.hash().getHex() << " height: " << download.getBestHeight() + startHeight + 1 << endl; });
         download.subscribeProtocolError([&](const string& error, int /*code*/)   { cout << "Protocol error:" << error << endl; });
 
         INIT_LOGGER("blockchain.log");
@@ -69,7 +75,7 @@ int main(int argc, char* argv[])
         signal(SIGINT, &finish);
         signal(SIGTERM, &finish);
 
-        download.start(host, port);
+        download.start(host, port, locatorHashes);
 
         while (!g_bShutdown) { usleep(200); }
         download.stop();
