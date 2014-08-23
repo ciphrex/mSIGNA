20b11e7bccfb9c62fbdeb59799c8210ad0d73f48
Eric Lombrozo <elombrozo@gmail.com>
2014-08-22 21:55:41 -0400

    v0.7.0

--- src/versioninfo.cpp
+++ src/versioninfo.cpp
@@ -19,7 +19,7 @@
 const int VERSIONPADDINGRIGHT = 20;
 const int VERSIONPADDINGBOTTOM = 30;
  
-const QString VERSIONTEXT("0.6.9 beta");
+const QString VERSIONTEXT("0.7.0 beta");
 
 const QString commitHash(COMMIT_HASH);
 const QString shortCommitHash(QString(COMMIT_HASH).left(7));
