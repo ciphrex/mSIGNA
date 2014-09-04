165fd6a15ffe9c1c9013c62b6b22777be96bf3c1
Eric Lombrozo <elombrozo@gmail.com>
2014-09-03 18:30:07 -0700

    Peer: increase read buffer size from 1024 to 16384 bytes.

--- deps/CoinQ/src/CoinQ_peer_io.h
+++ deps/CoinQ/src/CoinQ_peer_io.h
@@ -244,7 +244,7 @@ private:
 
     CoinQSignal<Peer&>                                  notifyTimeout;
 
-    static const unsigned int READ_BUFFER_SIZE = 1024;
+    static const unsigned int READ_BUFFER_SIZE = 16384;
     unsigned char read_buffer[READ_BUFFER_SIZE];
     std::size_t min_read_bytes;
 
