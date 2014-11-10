d1f1f31e2232622161c1fd8378f99b78287450a0
Eric Lombrozo <elombrozo@gmail.com>
2014-11-10 10:21:25 -0800

    Two usages for multibip32 tool - the first one just shows the address and pubkey for a single BIP32 master key child.

--- deps/CoinDB/tools/multibip32/src/multibip32.cpp
+++ deps/CoinDB/tools/multibip32/src/multibip32.cpp
@@ -22,16 +22,48 @@ using namespace std;
 
 const unsigned char ADDRESS_VERSIONS[] = { 0x00, 0x05 };
 
+void showUsage(char* argv[])
+{
+    cerr << "# Usage 1: " << argv[0] << " <master key> <path>" << endl;
+    cerr << "# Usage 2: " << argv[0] << " <minsigs> <master key 1> <path 1> ... [master key n] [path n]" << endl;
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc < 4 || argc % 2) // Parameter count must be even
+    if (argc < 3)
     {
-        cerr << "# Usage: " << argv[0] << " <minsigs> <master key 1> <path 1> ... [master key n] [path n]" << endl;
+        showUsage(argv);
         return -1;
     }
 
     try
     {
+        if (argc == 3)
+        {
+            bytes_t extkey;
+            if (!fromBase58Check(string(argv[1]), extkey)) throw runtime_error("Invalid master key base58.");
+
+            HDKeychain keychain(extkey);
+            keychain = keychain.getChild(string(argv[2]));
+
+            uchar_vector pubkey = keychain.pubkey();
+            vector<bytes_t> pubkeys;
+            pubkeys.push_back(pubkey);
+
+            Script script(Script::PAY_TO_PUBKEY_HASH, 1, pubkeys);
+            uchar_vector txoutscript = script.txoutscript();
+            cout << "TxOut script: " << txoutscript.getHex() << endl;
+            cout << "Address:      " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+            cout << "Public key:   " << pubkey.getHex() << endl;
+            return 0;
+        }
+
+        if (argc % 2)
+        {
+            showUsage(argv);
+            return -1;
+        }
+
         uint32_t minsigs = strtoul(argv[1], NULL, 10);
         vector<bytes_t> pubkeys;
 
@@ -49,6 +81,8 @@ int main(int argc, char* argv[])
             keychain = keychain.getChild(string(argv[i+1]));
             pubkeys.push_back(keychain.pubkey());
         }
+
+        //sort(pubkeys.begin(), pubkeys.end());
 
         Script script(Script::PAY_TO_MULTISIG_SCRIPT_HASH, minsigs, pubkeys);
         uchar_vector txoutscript = script.txoutscript();
