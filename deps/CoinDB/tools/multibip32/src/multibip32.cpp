07c0fab214b878172a1478ef62e01ab3e91d890d
Eric Lombrozo <elombrozo@gmail.com>
2014-11-10 14:35:34 -0800

    Also show uncompressed pubkey versions for multibip32 tool.

--- deps/CoinDB/tools/multibip32/src/multibip32.cpp
+++ deps/CoinDB/tools/multibip32/src/multibip32.cpp
@@ -46,15 +46,39 @@ int main(int argc, char* argv[])
             HDKeychain keychain(extkey);
             keychain = keychain.getChild(string(argv[2]));
 
-            uchar_vector pubkey = keychain.pubkey();
-            vector<bytes_t> pubkeys;
-            pubkeys.push_back(pubkey);
+            cout << endl;
 
-            Script script(Script::PAY_TO_PUBKEY_HASH, 1, pubkeys);
-            uchar_vector txoutscript = script.txoutscript();
-            cout << "TxOut script: " << txoutscript.getHex() << endl;
-            cout << "Address:      " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
-            cout << "Public key:   " << pubkey.getHex() << endl;
+            {
+                uchar_vector pubkey = keychain.pubkey();
+                vector<bytes_t> pubkeys;
+                pubkeys.push_back(pubkey);
+
+                Script script(Script::PAY_TO_PUBKEY_HASH, 1, pubkeys);
+                uchar_vector txoutscript = script.txoutscript();
+                cout << "Compressed TxOut Script:   " << txoutscript.getHex() << endl;
+                cout << "Compressed Address:        " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+                cout << "Compressed Public Key:     " << pubkey.getHex() << endl;
+                cout << endl;
+            }
+
+            { 
+                uchar_vector pubkey = keychain.uncompressed_pubkey();
+                vector<bytes_t> pubkeys;
+                pubkeys.push_back(pubkey);
+
+                Script script(Script::PAY_TO_PUBKEY_HASH, 1, pubkeys);
+                uchar_vector txoutscript = script.txoutscript();
+                cout << "Uncompressed TxOut Script: " << txoutscript.getHex() << endl;
+                cout << "Uncompressed Address:      " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+                cout << "Uncompressed Public Key:   " << pubkey.getHex() << endl;
+                cout << endl;
+            }
+
+            if (keychain.isPrivate())
+            {
+                cout << "Private key:               " << uchar_vector(keychain.privkey()).getHex() << endl;
+                cout << endl;
+            }
             return 0;
         }
 
@@ -66,6 +90,7 @@ int main(int argc, char* argv[])
 
         uint32_t minsigs = strtoul(argv[1], NULL, 10);
         vector<bytes_t> pubkeys;
+        vector<bytes_t> uncompressed_pubkeys;
 
         for (size_t i = 2; i < argc; i+=2)
         {
@@ -80,14 +105,29 @@ int main(int argc, char* argv[])
             HDKeychain keychain(extkey);
             keychain = keychain.getChild(string(argv[i+1]));
             pubkeys.push_back(keychain.pubkey());
+            uncompressed_pubkeys.push_back(keychain.uncompressed_pubkey());
         }
 
-        //sort(pubkeys.begin(), pubkeys.end());
+        sort(pubkeys.begin(), pubkeys.end());
+        sort(uncompressed_pubkeys.begin(), uncompressed_pubkeys.end());
 
-        Script script(Script::PAY_TO_MULTISIG_SCRIPT_HASH, minsigs, pubkeys);
-        uchar_vector txoutscript = script.txoutscript();
-        cout << "TxOut script: " << txoutscript.getHex() << endl;
-        cout << "Address:      " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+        cout << endl;
+
+        {
+            Script script(Script::PAY_TO_MULTISIG_SCRIPT_HASH, minsigs, pubkeys);
+            uchar_vector txoutscript = script.txoutscript();
+            cout << "Compressed TxOut Script:   " << txoutscript.getHex() << endl;
+            cout << "Compressed Address:        " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+            cout << endl;
+        }
+
+        {
+            Script script(Script::PAY_TO_MULTISIG_SCRIPT_HASH, minsigs, uncompressed_pubkeys);
+            uchar_vector txoutscript = script.txoutscript();
+            cout << "Uncompressed TxOut Script: " << txoutscript.getHex() << endl;
+            cout << "Uncompressed Address:      " << getAddressForTxOutScript(txoutscript, ADDRESS_VERSIONS) << endl;
+            cout << endl;
+        }
     }
     catch (const exception& e)
     {
