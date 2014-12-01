22db723073bacbdd6d1875b07f51ea898a821215
Eric Lombrozo <elombrozo@gmail.com>
2014-11-30 23:45:42 -0800

    v0.8.11

--- src/versioninfo.cpp
+++ src/versioninfo.cpp
@@ -16,7 +16,7 @@
 #include <openssl/opensslv.h>
 
 // Definitions
-const QString VERSIONTEXT("0.8.10 beta");
+const QString VERSIONTEXT("0.8.11 beta");
 
 const QString commitHash(COMMIT_HASH);
 const QString shortCommitHash(QString(COMMIT_HASH).left(7));
