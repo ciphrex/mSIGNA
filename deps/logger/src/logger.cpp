931da34b95f1cc6bac165d9fb6d1c7f0a903b420
Eric Lombrozo <elombrozo@gmail.com>
2015-02-16 23:35:24 -0800

    Added UTC timestamp to logger.

--- deps/logger/src/logger.cpp
+++ deps/logger/src/logger.cpp
@@ -23,13 +23,26 @@
 // THE SOFTWARE.
 
 #include "logger.h"
+
 #include <fstream>
+#include <time.h>
 
 namespace logger {
     std::ofstream file;
     void init_logger(const char* filename)
     {
         file.open(filename, std::ios_base::app);
+    }
+
+    std::string timestamp()
+    {
+        time_t rawtime;
+        time(&rawtime);
+        struct tm* timeinfo = gmtime(&rawtime);
+
+        char buffer[20];
+        strftime(buffer, 20, "%F %T",timeinfo);
+        return std::string(buffer);
     }
 
     std::ostream out(file.rdbuf());
