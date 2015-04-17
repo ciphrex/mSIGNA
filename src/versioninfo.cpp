79a82a6003a6e7aa919a034cbc2078317fa1f824
Eric Lombrozo <elombrozo@gmail.com>
2015-04-17 01:04:17 -0700

    v0.9.1

--- src/versioninfo.cpp
+++ src/versioninfo.cpp
@@ -16,7 +16,7 @@
 #include <openssl/opensslv.h>
 
 // Definitions
-const QString VERSIONTEXT("0.9.0 beta");
+const QString VERSIONTEXT("0.9.1 beta");
 
 const QString commitHash(COMMIT_HASH);
 const QString shortCommitHash(QString(COMMIT_HASH).left(7));
