918e9f58d6ce53b4403e73da95589814074c52bf
Eric Lombrozo <elombrozo@gmail.com>
2016-01-02 23:36:14 -0800

    Added witness template example.

--- deps/CoinQ/examples/script/src/templates.cpp
+++ deps/CoinQ/examples/script/src/templates.cpp
@@ -27,8 +27,9 @@ string help(char* appName)
 {
     stringstream ss;
     ss  << "# Usage: " << appName << " <type> [params]" << endl
-        << "#   p2pkh <pubkey>" << endl
-        << "#   mofn <m> <pubkey1> [pubkey2] ...";
+        << "#   p2pkh   <pubkey>" << endl
+        << "#   mofn    <m> <pubkey1> [pubkey2] ..." << endl
+        << "#   witness <redeemscript>";
     return ss.str();
 }
 
@@ -50,16 +51,16 @@ int main(int argc, char* argv[])
             uchar_vector pubkey(argv[2]);
             if (pubkey.size() != 33) throw runtime_error("Invalid pubkey length.");
 
-            uchar_vector tokenPubKey;
-            tokenPubKey << OP_DUP << OP_HASH160 << OP_PUBKEYHASH << 0 << OP_EQUALVERIFY << OP_CHECKSIG;
-            ScriptTemplate scriptPubKeyTemplate(tokenPubKey);
-            cout << "scriptPubKey:  " << scriptPubKeyTemplate.script(pubkey).getHex() << endl;
+            uchar_vector outPattern;
+            outPattern << OP_DUP << OP_HASH160 << OP_PUBKEYHASH << 0 << OP_EQUALVERIFY << OP_CHECKSIG;
+            ScriptTemplate outTemplate(outPattern);
 
-            uchar_vector tokenSig;
-            tokenSig << OP_0 << OP_PUBKEY << 0;
-            ScriptTemplate scriptSigTemplate(tokenSig);
-            cout << "scriptSig:     " << scriptSigTemplate.script(pubkey).getHex() << endl;
+            uchar_vector inPattern;
+            inPattern << OP_0 << OP_PUBKEY << 0;
+            ScriptTemplate inTemplate(inPattern);
 
+            cout << "txoutscript:   " << outTemplate.script(pubkey).getHex() << endl;
+            cout << "txinscript:    " << inTemplate.script(pubkey).getHex() << endl;
             cout << "address:       " << toBase58Check(hash160(pubkey), ADDRESS_VERSIONS[0]) << endl;
         }
         else if (type == "mofn")
@@ -81,26 +82,41 @@ int main(int argc, char* argv[])
             if ((int)pubkeys.size() < minsigs || pubkeys.size() > 15)
                 throw runtime_error("Invalid pubkey count.");
 
-            uchar_vector tokenRedeemScript;
-            tokenRedeemScript << (OP_1_OFFSET + minsigs);
+            uchar_vector redeemPattern, emptySigs;
+            redeemPattern << (OP_1_OFFSET + minsigs);
             for (size_t i = 0; i < pubkeys.size(); i++)
             {
-                tokenRedeemScript << OP_PUBKEY << i;
+                redeemPattern << OP_PUBKEY << i;
+                emptySigs << OP_0;
             }
-            tokenRedeemScript << (OP_1_OFFSET + pubkeys.size());
+            redeemPattern << (OP_1_OFFSET + pubkeys.size());
 
             SymmetricKeyGroup keyGroup(pubkeys);
-            ScriptTemplate redeemscriptTemplate(tokenRedeemScript);
-            uchar_vector redeemscript = redeemscriptTemplate.script(keyGroup.pubkeys());
+            ScriptTemplate redeemTemplate(redeemPattern);
+            uchar_vector redeemscript = redeemTemplate.script(keyGroup.pubkeys());
 
-            cout << "redeemscript: " << redeemscript.getHex() << endl;
+            uchar_vector txoutscript;
+            txoutscript << OP_HASH160 << pushStackItem(hash160(redeemscript)) << OP_EQUAL;
 
-            uchar_vector scriptPubKey;
-            scriptPubKey << OP_HASH160 << pushStackItem(hash160(redeemscript)) << OP_EQUAL;
+            uchar_vector txinscript;
+            txinscript << OP_0 << emptySigs << pushStackItem(redeemscript);
 
-            cout << "scriptPubKey: " << scriptPubKey.getHex() << endl;
+            cout << "redeemscript:  " << redeemscript.getHex() << endl;
+            cout << "txoutscript:   " << txoutscript.getHex() << endl;
+            cout << "txinscript:    " << txinscript.getHex() << endl;
+            cout << "address:       " << toBase58Check(hash160(redeemscript), ADDRESS_VERSIONS[1]) << endl;
+        }
+        else if (type == "witness")
+        {
+            if (argc != 3) throw runtime_error(help(argv[0]));
 
-            cout << "address:      " << toBase58Check(hash160(redeemscript), ADDRESS_VERSIONS[1]) << endl;
+            uchar_vector redeemscript(argv[2]);
+            WitnessProgram wp(redeemscript);
+            cout << "witnessscript: " << wp.witnessscript().getHex() << endl;
+            cout << "redeemscript:  " << wp.redeemscript().getHex() << endl;
+            cout << "txoutscript:   " << wp.txoutscript().getHex() << endl;
+            cout << "txinscript:    " << wp.txinscript(). getHex() << endl;
+            cout << "address:       " << wp.address(ADDRESS_VERSIONS) << endl;
         }
         else
         {
