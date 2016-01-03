fab8703c9707c943e9466422ccebc7297815cec0
Eric Lombrozo <elombrozo@gmail.com>
2016-01-02 21:51:46 -0800

    m-of-n script template.

--- deps/CoinQ/examples/script/src/templates.cpp
+++ deps/CoinQ/examples/script/src/templates.cpp
@@ -64,6 +64,43 @@ int main(int argc, char* argv[])
         }
         else if (type == "mofn")
         {
+            if (argc < 4) throw runtime_error(help(argv[0]));
+
+            int minsigs = strtol(argv[2], NULL, 0);
+            if (minsigs < 1 || minsigs > 15)
+                throw runtime_error("Invalid minsigs.");
+
+            vector<bytes_t> pubkeys;
+            for (int i = 3; i < argc; i++)
+            {
+                uchar_vector pubkey(argv[i]);
+                if (pubkey.size() != 33) throw runtime_error("Invalid pubkey length.");
+
+                pubkeys.push_back(pubkey);
+            }
+            if ((int)pubkeys.size() < minsigs || pubkeys.size() > 15)
+                throw runtime_error("Invalid pubkey count.");
+
+            uchar_vector tokenRedeemScript;
+            tokenRedeemScript << (OP_1_OFFSET + minsigs);
+            for (size_t i = 0; i < pubkeys.size(); i++)
+            {
+                tokenRedeemScript << OP_PUBKEY << i;
+            }
+            tokenRedeemScript << (OP_1_OFFSET + pubkeys.size());
+
+            SymmetricKeyGroup keyGroup(pubkeys);
+            ScriptTemplate redeemscriptTemplate(tokenRedeemScript);
+            uchar_vector redeemscript = redeemscriptTemplate.script(keyGroup.pubkeys());
+
+            cout << "redeemscript: " << redeemscript.getHex() << endl;
+
+            uchar_vector scriptPubKey;
+            scriptPubKey << OP_HASH160 << pushStackItem(hash160(redeemscript)) << OP_EQUAL;
+
+            cout << "scriptPubKey: " << scriptPubKey.getHex() << endl;
+
+            cout << "address:      " << toBase58Check(hash160(redeemscript), ADDRESS_VERSIONS[1]) << endl;
         }
         else
         {
