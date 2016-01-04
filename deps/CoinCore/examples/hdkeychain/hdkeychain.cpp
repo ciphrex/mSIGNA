96d3832acfd6517342fd00e89bbd6058ac12e46f
Eric Lombrozo <elombrozo@gmail.com>
2016-01-03 23:57:05 -0800

    Updated the hdkeychain utility to output extended keys.

--- deps/CoinCore/examples/hdkeychain/hdkeychain.cpp
+++ deps/CoinCore/examples/hdkeychain/hdkeychain.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     if (argc < 3) {
-        cout << "Usage: " << argv[0] << " <extended key> <child num>" << endl;
+        cout << "Usage: " << argv[0] << " <extended key> <child num> [extended = 0]" << endl;
         return 0;
     }
 
@@ -23,10 +23,19 @@ int main(int argc, char* argv[])
 
         uint32_t childnum = strtoull(argv[2], NULL, 0);
 
+        bool bExtended = argc > 3 && strtol(argv[3], NULL, 0);
+
         HDKeychain hdkeychain(extkey);
         hdkeychain = hdkeychain.getChild(childnum);
 
-        cout << uchar_vector(hdkeychain.key()).getHex() << endl;
+        if (bExtended)
+        {
+            cout << toBase58Check(hdkeychain.extkey()) << endl;
+        }
+        else
+        {
+            cout << uchar_vector(hdkeychain.key()).getHex() << endl;
+        }
     }
     catch (const exception& e) {
         cerr << "Error: " << e.what() << endl;
