858a7c3d6a027f858967ddce817cf72bdee2a91d
Eric Lombrozo <elombrozo@gmail.com>
2017-05-03 02:02:12 -0700

    Updated copyright info to 2017.

--- src/copyrightinfo.cpp
+++ src/copyrightinfo.cpp
@@ -4,8 +4,8 @@
 //
 // copyrightinfo.cpp
 //
-// Copyright (c) 2013-2016 Eric Lombrozo
-// Copyright (c) 2011-2016 Ciphrex Corp.
+// Copyright (c) 2013-2017 Eric Lombrozo
+// Copyright (c) 2011-2017 Ciphrex Corp.
 //
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
@@ -14,7 +14,7 @@
 #include "copyrightinfo.h"
 
 // Definitions
-const QString COPYRIGHTTEXT("Copyright (c) 2013-2016 Ciphrex Corporation, All Rights Reserved");
+const QString COPYRIGHTTEXT("Copyright (c) 2013-2017 Ciphrex Corporation, All Rights Reserved");
 
 // Accessors
 const QString& getCopyrightText() { return COPYRIGHTTEXT; }
